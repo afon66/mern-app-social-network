6d6cdc6cbb2136f9dba19019188280f57b9fe465
Afanasi <afanasi909@gmail.com>
2025-03-28 05:05:39 +0200

    Update constants.ts

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = process.env.NODE_ENV === 'production' ? 'none' : 'https://mern-app-server-1-xnha.onrender.com'
+export const BASE_URL = process.env.NODE_ENV === 'production' ? 'none' : 'https://mern-app-server-9h7nfff.vercel.app/'
